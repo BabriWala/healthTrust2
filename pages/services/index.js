90ae55f85969463cd811f9cbb00bd176e0cccc30
mehedy3107 <mhador111@gmail.com>
2022-12-11 21:20:30 +0600

    Services Page testing

--- pages/services/index.js
+++ pages/services/index.js
@@ -1,9 +1,10 @@
 const index = () => {
-    return (
-        <div>
-           <h2>This is Services Page</h2> 
-        </div>
-    );
+  return (
+    <div>
+      <h2>This is Services Page</h2>
+      <h2>I will add soon</h2>
+    </div>
+  );
 };
 
 export default index;
